ba65bf6adec36828983e32752bfa4b714ff58bdc
Yashaswi <44907218+yashpupneja@users.noreply.github.com>
2023-12-13 15:28:29 -0500

    Update bootstrap_training.py

--- bootstrap_training.py
+++ bootstrap_training.py
@@ -1,105 +1,86 @@
+import re
 import numpy as np
-from sklearn.base import clone
-from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.utils import resample
-from tqdm import tqdm
-from model_baseline import train_test_data_split, vectorize, train_and_evaluate
+import pickle
+from nltk.tokenize import word_tokenize
 from preprocess import get_instances, get_labels
-from scipy.sparse import vstack, hstack, csr_matrix
-from sklearn.preprocessing import StandardScaler
+from seedsample import uniform_split
+from scipy.sparse import vstack
+import random
 
-# Mockup for vectorization function to be replaced with actual preprocessing logic
-def vectorize_data(X, y):
-    vectorizer = TfidfVectorizer()
-    X_vectorized = vectorizer.fit_transform(X)
-    return X_vectorized, y
+def preprocessor(s):
+    s = s.lower()
+    s = re.sub(r'[^a-zA-Z]', ' ', s)
+    s = " ".join(s.split())
+    return s
 
-def yarowsky_bootstrap_corrected(initial_model, X_labeled, y_labeled, X_unlabeled, threshold=0.9, max_iterations=100):
-    model = clone(initial_model)
-    # Ensure y_labeled is a numpy array for proper indexing
-    y_labeled = np.array(y_labeled)
+def yarowsky_bootstrapping(labeled_sentences, labels, unlabeled_sentences, threshold=0.75):
+    # Initialize vectorizer without a preprocessor since data is already preprocessed
+    vectorizer = TfidfVectorizer(lowercase=True, tokenizer=word_tokenize)
+    X_labeled = vectorizer.fit_transform(labeled_sentences)
+    y_labeled = np.array(labels)
 
-    # Initialize an array to keep track of which samples are currently labeled
-    labeled_indices = np.arange(X_labeled.shape[0])
+    # Scale the data
+    scaler = StandardScaler(with_mean=False)  # Use with_mean=False for sparse matrices
+    X_labeled = scaler.fit_transform(X_labeled)
 
-    # Combine labeled and unlabeled data for easy indexing
-    X_combined = vstack([X_labeled, X_unlabeled])
-    y_combined = np.concatenate([y_labeled, np.zeros(X_unlabeled.shape[0])])  # Placeholder zeros for unlabeled data
+    #classifier = LogisticRegression(max_iter=1000)
+    classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+    unlabeled_indices = list(range(len(unlabeled_sentences)))
 
-    for iteration in range(max_iterations):
-        # Fit the model on the currently labeled data
-        X_train = X_combined[labeled_indices].toarray() if isinstance(X_combined, csr_matrix) else X_combined[labeled_indices]
-        y_train = y_combined[labeled_indices]  # Indexing with a numpy array
-        # Optionally, scale the data
-        scaler = StandardScaler(with_mean=False)  # Use with_mean=False for sparse matrices
-        X_train_scaled = scaler.fit_transform(X_train)
+    while unlabeled_indices:
+        classifier.fit(X_labeled, y_labeled)
 
-        # Fit the model on the currently labeled data
-        model.fit(X_train_scaled, y_train)  # Use scaled data
-        # model.fit(X_train, y_train)
-        
-        # Predict probabilities on the unlabeled data
-        unlabeled_indices = [i for i in range(X_combined.shape[0]) if i not in labeled_indices]
-        X_test_unlabeled = X_combined[unlabeled_indices].toarray() if isinstance(X_combined, csr_matrix) else X_combined[unlabeled_indices]
-        proba = model.predict_proba(X_test_unlabeled)
-        
-        # Find the instances in the unlabeled data with a predicted probability above the threshold
-        confident_indices = np.flatnonzero(np.max(proba, axis=1) > threshold)
-        actual_confident_indices = [unlabeled_indices[i] for i in confident_indices]
+        processed_unlabeled = [preprocessor(unlabeled_sentences[i]) for i in unlabeled_indices]
+        X_unlabeled = vectorizer.transform(processed_unlabeled)
+        proba = classifier.predict_proba(X_unlabeled)
+        max_proba = np.max(proba, axis=1)
+        confident_indices = [i for i, p in enumerate(max_proba) if p >= threshold]
 
-        # Break the loop if no confident predictions are made
-        if not actual_confident_indices:
+        if not confident_indices:
             break
 
-        # Add the confident predictions to the labeled dataset
+        X_confident = X_unlabeled[confident_indices]
         y_confident = np.argmax(proba[confident_indices], axis=1)
-        y_combined[actual_confident_indices] = y_confident
 
-        # Update the labeled indices
-        labeled_indices = np.concatenate([labeled_indices, actual_confident_indices])
+        X_labeled = vstack([X_labeled, X_confident])
+        y_labeled = np.concatenate([y_labeled, y_confident])
 
-        print(f"Iteration {iteration}: {len(confident_indices)} new instances added.")
-    
-    return model, X_combined[labeled_indices], y_combined[labeled_indices]
+        unlabeled_indices = [i for i in unlabeled_indices if i not in confident_indices]
 
-# Get unlabeled instances
-def get_unlabeled_instances():
-    f = open(f'dataset/WOS/WOS11967/X.txt')
-    x = f.read().splitlines()
-    return x
+    return classifier
 
-# The main function to run the bootstrapping process
 def main():
-    X_labeled = get_instances()  # Replace with actual data retrieval
-    y_labeled = get_labels()     # Replace with actual data retrieval
-    X_unlabeled = get_unlabeled_instances()  # Replace with actual data retrieval
+    # Assuming get_instances() and get_labels() fetch your initial dataset
+    x, y = get_instances(), get_labels()
+    
+    # Assuming uniform_split is defined in seedsample.py
+    seed_x, seed_y = uniform_split(x, y, 1)  # Adjust the per_class_size as needed
 
-    # Vectorize the data
-    X_vectorized, y_encoded = vectorize_data(X_labeled, y_labeled)
-    X_unlabeled_vectorized, _ = vectorize_data(X_unlabeled, None)  # No labels for unlabeled data
+    unlabeled_sentences = [sentence for sentence in x if sentence not in seed_x]
+    # Process each sentence individually
+    processed_seed_x = [preprocessor(sentence) for sentence in seed_x]
+    processed_unlabeled_sentences = [preprocessor(sentence) for sentence in unlabeled_sentences]
 
-    # Initialize the model
-    # initial_model = LogisticRegression(max_iter=100)
-    initial_model = LogisticRegression(max_iter=1000, solver='saga', C=1.0)
+    classifier = yarowsky_bootstrapping(processed_seed_x, seed_y, processed_unlabeled_sentences, threshold=0.75)
 
-    # Run Yarowsky bootstrapping
-    refined_model, X_refined, y_refined = yarowsky_bootstrap_corrected(
-        initial_model, X_vectorized, y_encoded, X_unlabeled_vectorized
-    )
+    # Evaluation and saving the classifier
+    vectorizer = TfidfVectorizer(lowercase=True, tokenizer=word_tokenize)
+    X = vectorizer.fit_transform(x)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Split the refined data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X_refined, y_refined, test_size=0.2, random_state=42)
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
+    print(classification_report(y_test, y_pred))
 
-    # Train the refined model on the training set
-    refined_model.fit(X_train, y_train)
-
-    # Evaluate the refined model on the testing set
-    predictions = refined_model.predict(X_test)
-    accuracy = accuracy_score(y_test, predictions)
-    print(f"Accuracy of the refined Logistic Regression model is: {accuracy}")
+    with open('classifier_model.pkl', 'wb') as file:
+        pickle.dump(classifier, file)
 
 if __name__ == "__main__":
     main()
